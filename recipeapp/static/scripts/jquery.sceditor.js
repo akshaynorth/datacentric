4d34df770106ada47068ad79339c7675a382bcd4
DA A <ado_north@outlook.com>
2020-11-26 00:40:00 -0500

    updated user interactin to add receipe instructions

--- recipeapp/static/scripts/jquery.sceditor.js
+++ recipeapp/static/scripts/jquery.sceditor.js
@@ -16,6 +16,12 @@
 			newElem.appendTo('table#ingredients-sort');
 		}
 
+		function addInstruction() {
+			var newElem = $('table#instructions-sort tr.ingredients-cont.ing:first').clone();
+			newElem.find('input').val('');
+			newElem.appendTo('table#instructions-sort');
+		}
+
 		//sortable table
 		var fixHelper =  function(e, tr) {
 		    var $originals = tr.children();
@@ -28,12 +34,12 @@
 		    return $helper;
 		  };
 		if ($("table#ingredients-sort").is('*')) {
-			$('.add_ingredient').click(function(e){
+			$('table#ingredients-sort + .add_ingredient').click(function(e){
 				e.preventDefault();
 				addIng();
 			})
 
-			$('.add_separator').click(function(e){
+			$('table#ingredients-sort + .add_separator').click(function(e){
 				e.preventDefault();
 				var newElem = $('<tr class="ingredients-cont separator"><td class="icon"><i class="fa fa-arrows"></i></td><td><input name="ingredient_name" type="text" class="ingredient" placeholder="" /></td><td><input name="ingredient_note" type="text" class="notes" placeholder="Separator" /></td><td class="action"><a title="Delete" class="delete" href="#"><i class="fa fa-remove"></i></a></td></tr>');
 				newElem.appendTo('table#ingredients-sort');
@@ -45,6 +51,34 @@
 			});
 
 			$('table#ingredients-sort tbody').sortable({
+				forcePlaceholderSize: true,
+				forceHelperSize: true,
+				placeholder : 'sortableHelper',
+				helper: fixHelper,
+				zIndex: 999990,
+				opacity: 0.6,
+				tolerance: "pointer"
+			})
+
+			// Adding recipe instructions
+           if ($("table#instructions-sort").is('*')) {
+			$('table#instructions-sort + .add_ingredient').click(function(e){
+				e.preventDefault();
+				addInstruction();
+			})
+
+			$('table#instructions-sort + .add_separator').click(function(e){
+				e.preventDefault();
+				var newElem = $('<tr class="ingredients-cont separator"><td class="icon"><i class="fa fa-arrows"></i></td><td><input name="instruction_desc" type="text" class="ingredient" placeholder="" /></td><td class="action"><a title="Delete" class="delete" href="#"><i class="fa fa-remove"></i></a></td></tr>');
+				newElem.appendTo('table#instructions-sort');
+			})
+			// remove instruction
+			$('#instructions-sort .delete').live('click',function(e){
+				e.preventDefault();
+				$(this).parent().parent().remove();
+			});
+
+			$('table#instructions-sort tbody').sortable({
 				forcePlaceholderSize: true,
 				forceHelperSize: true,
 				placeholder : 'sortableHelper',
