d3e053d158c1349694848c89976b8f41c73bc1f1
DA A <ado_north@outlook.com>
2020-12-01 01:28:14 -0500

    added code to retrieve receip from database for homepage display

--- recipeapp/recipeapp.py
+++ recipeapp/recipeapp.py
@@ -30,6 +30,24 @@ bp = Blueprint('recipeapp', __name__)
 @bp.route('/', defaults={'page': 'index.html'}, methods=('GET', 'POST'))
 @bp.route('/<page>', methods=('GET', 'POST'))
 def show_page(page):
+
+    if page == 'index.html':
+        client = pymongo.MongoClient(os.environ['MONGODB_URI'])
+        db = client.get_default_database()
+        recipe_collection = db['recipes']
+        # Get the latest recipes added to the database for home page display
+        recipe_cursor = recipe_collection.find().sort('date', pymongo.DESCENDING).limit(9)
+
+        recipe_list = []
+        for recipe in recipe_cursor:
+            recipe_list.append(recipe)
+
+        client.close()
+        return render_template(
+            'recipeapp/{}'.format(page),
+            repipe_list=recipe_list
+        )
+
     return render_template(
         'recipeapp/{}'.format(page)
     )
