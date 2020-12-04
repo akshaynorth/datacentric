49c612023f25cd26afbefc00fce9a418c3d08cc0
DA A <ado_north@outlook.com>
2020-12-03 23:29:01 -0500

    Added generate recipe button collection of recipe type

--- recipeapp/static/scripts/recipe_rest.js
+++ recipeapp/static/scripts/recipe_rest.js
@@ -2,12 +2,19 @@
 	$(document).ready(function(){
 	    function create_recipe() {
 	        // Gather all the information on the generate cooking directions form
-            var recipe_label = $('#recipe_label').val().trim()
+            let recipe_label = $('#recipe_label').val().trim()
 
             if (recipe_label.length == 0) {
                 alert('Please provide a recipe label.');
                 return;
             }
+
+            let recipe_type = $('#recipe_type').val().trim();
+            if (recipe_type.length == 0) {
+                alert('Please provide a selection for the recipe type.')
+                return;
+            }
+
 	        // Send a request to the flask application to create the recipe
 	    }
 
