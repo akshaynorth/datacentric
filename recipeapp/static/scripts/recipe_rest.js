2cfd107f32b44fae8a4925bc4af53fea7aad9746
DA A <ado_north@outlook.com>
2020-12-03 23:05:22 -0500

    Added Javascript code to collect value for the recipe label on create recipe button click

---
+++ recipeapp/static/scripts/recipe_rest.js
@@ -0,0 +1,20 @@
+(function($){
+	$(document).ready(function(){
+	    function create_recipe() {
+	        // Gather all the information on the generate cooking directions form
+            var recipe_label = $('#recipe_label').val().trim()
+
+            if (recipe_label.length == 0) {
+                alert('Please provide a recipe label.');
+                return;
+            }
+	        // Send a request to the flask application to create the recipe
+	    }
+
+	    $('#create_recipe_btn').click(function(e) {
+	        e.preventDefault();
+            create_recipe();
+	    })
+	}
+	})
+)(this.jQuery);
